fb85a716de090b16b484c0d8df463ae5c8ef48a2
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2025-01-31 19:55:06 +0200

    change date

--- src/js/changeDate.js
+++ src/js/changeDate.js
@@ -1,17 +1,28 @@
 // change day
 
 var currentDate = new Date();
+// Увеличение значения дня на 1
 currentDate.setDate(currentDate.getDate() + 1);
+// Получение отдельных составляющих даты
 var day = currentDate.getDate();
-var month = currentDate.getMonth() + 1;
+var month = currentDate.getMonth() + 1; // Месяцы в JavaScript начинаются с 0, поэтому добавляем 1
 var year = currentDate.getFullYear();
-var formattedDate = day;
-document.getElementById('dateElement').innerHTML = formattedDate;
+// Форматирование даты в нужном формате (например, "дд.мм.гггг")
+
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+var formattedDate = addLeadingZero(day);
+// Обновление значения на вашем сайте
+document.getElementById('dateElement').innerHTML = day;
 
 // **************************************************************************
 
+// change month
 var current_date = new Date();
 current_date.setDate(current_date.getDate() + 1);
+
 // var month = new Array(
 //   'ЯНВАРЯ',
 //   'ФЕВРАЛЯ',
@@ -27,21 +38,6 @@ current_date.setDate(current_date.getDate() + 1);
 //   'ДЕКАБРЯ'
 // );
 
-var month = new Array(
-  '01',
-  '02',
-  '03',
-  '04',
-  '05',
-  '06',
-  '07',
-  '08',
-  '09',
-  '10',
-  '11',
-  '12'
-);
-
 // var month = new Array(
 //   'СІЧНЯ',
 //   'ЛЮТОГО',
@@ -56,5 +52,21 @@ var month = new Array(
 //   'ЛИСТОПАДА',
 //   'ГРУДНЯ'
 // );
+
 document.getElementById('monthElement').innerHTML =
   month[current_date.getMonth()];
+
+var month = new Array(
+  '01',
+  '02',
+  '03',
+  '04',
+  '05',
+  '06',
+  '07',
+  '08',
+  '09',
+  '10',
+  '11',
+  '12'
+);
